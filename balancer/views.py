964beb8078ca2afe08003a6cf7fb277dc108a2c6
Edmund Chong @ Banana Senpai <7440820@gmail.com>
2019-03-09 14:32:45 +1100

    The student creation form is now built pseudo-manually. We are probably going to remove the ListView buffoonery since that is just plain ugly.
    Signed-off-by: Edmund Chong @ Banana Senpai <7440820@gmail.com>

--- balancer/views.py
+++ balancer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from balancer.models import Student
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from balancer.forms import StudentForm
+import sys
 
 def index(request):
     num_students = Student.objects.all().count()
@@ -24,3 +27,33 @@ class StudentDetailView(generic.DetailView):
         # Add in a QuerySet of all the books
         # context['book_list'] = Book.objects.all()
         return context
+
+# class StudentCreate(CreateView):
+    # model = Student
+    # fields = ['first_name', 'surname', 'given_name', 'balance']
+
+
+def StudentCreate(request):
+    form = None
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            student_hash = {
+                'created_by':'edmundsuperadmin',
+                'created_date':"2019-02-17"
+            }
+
+            for field in form.cleaned_data:
+                student_hash[field] = form.cleaned_data[field]
+            record = Student(**student_hash)
+            record.save()
+    else:
+        form = StudentForm(initial={'balance': 0.0})
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'balancer/student_form.html', context)
