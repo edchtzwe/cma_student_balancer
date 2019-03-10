9a9b3bfdf82ed8cd1c72850ab1cc8039db18990b
Edmund Chong @ Banana Senpai <7440820@gmail.com>
2019-03-10 20:02:59 +1100

    Index plus is usually called after an operation where we want the index page, but with more feedback.
    Signed-off-by: Edmund Chong @ Banana Senpai <7440820@gmail.com>

--- balancer/urls.py
+++ balancer/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('(?P<result>\S+)', views.index_plus, name='index-plus'),
     re_path('student/(?P<pk>\d+)', views.StudentDetailView.as_view(), name='student-detail'),
     path('student/create/', views.StudentCreate, name='student-create'),
+    re_path('student/operation/', views.StudentModifyDelete, name='student-operation'),
 ]
