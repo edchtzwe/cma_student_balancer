2519d842121ec65b454fb0a6e86355b1f24ce8ef
Edmund Chong @ Banana Senpai <7440820@gmail.com>
2019-03-11 13:54:34 +1100

    Silencing the error we get from using path with regex

--- balancer/urls.py
+++ balancer/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('(?P<result>\S+)', views.index_plus, name='index-plus'),
+    re_path('(?P<result>\S+)', views.index_plus, name='index-plus'),
     re_path('student/(?P<pk>\d+)', views.StudentDetailView.as_view(), name='student-detail'),
     path('student/create/', views.StudentCreate, name='student-create'),
     re_path('student/operation/', views.StudentModifyDelete, name='student-operation'),
