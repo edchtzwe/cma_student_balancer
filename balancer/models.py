fc70c456fedd2bf0badbf9d609278239e312a4ac
Edmund Chong @ Banana Senpai <7440820@gmail.com>
2019-03-30 14:20:09 +1100

    Code cleaning for readability. Define memeber functions that return a list of iterable fields to make accessing member elements possible
    Signed-off-by: Edmund Chong @ Banana Senpai <7440820@gmail.com>

--- balancer/models.py
+++ balancer/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.urls import reverse  # To generate URLS by reversing URL patterns
 
 class Student(models.Model):
-    student_id = models.CharField(max_length=512)
+    student_id        = models.CharField(max_length=512)
     # data fields
-    first_name = models.CharField(max_length=256)
-    surname = models.CharField(max_length=256)
-    given_name = models.CharField(max_length=256)
-    belt_color = models.CharField(max_length=256)
-    dan_level = models.IntegerField(blank=True, null=True)
-    balance = models.FloatField(default=0.0)
+    first_name        = models.CharField(max_length=256)
+    surname           = models.CharField(max_length=256)
+    given_name        = models.CharField(max_length=256)
+    belt_color        = models.CharField(max_length=256)
+    dan_level         = models.IntegerField(blank  =True, null=True)
+    balance           = models.FloatField(default  =0.0)
 
     # audit
-    created_by = models.CharField(max_length=256)
-    created_date = models.DateField()
-    last_modified_by = models.CharField(max_length=256, blank=True, null=True)
-    last_modified_date = models.DateField(blank=True, null=True)
-    last_action = models.CharField(max_length=256, blank=True, null=True)
+    created_by        = models.CharField(max_length=256)
+    created_date      = models.DateField()
+    last_modified_by  = models.CharField(max_length=256, blank=True, null=True)
+    last_modified_date= models.DateField(blank     =True, null=True)
+    last_action       = models.CharField(max_length=256, blank=True, null=True)
 
     # metadata
     class Meta:
@@ -29,3 +29,31 @@ class Student(models.Model):
     def get_absolute_url(self):
         return reverse('student-detail', args=[str(self.pk)])
 
+    # this should return everything, this needs strict permission controls
+    def get_all_fields_as_iterable(self):
+        fields = [
+            'student_id'        ,
+            'first_name'        ,
+            'surname'           ,
+            'given_name'        ,
+            'belt_color'        ,
+            'dan_level'         ,
+            'balance'           ,
+            'created_by'        ,
+            'created_date'      ,
+            'last_modified_by'  ,
+            'last_modified_date',
+            'last_action'       ,
+        ]
+        return fields
+
+    # this will be what's available to most users
+    def get_iterable_fields(self):
+        fields = [
+            'student_id',
+            'first_name',
+            'surname'   ,
+            'given_name',
+            'balance'   ,
+        ]
+        return fields
