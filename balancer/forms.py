21f73d942a5c6a497eafe889093fb84d29c8b494
Edmund Chong @ Banana Senpai <7440820@gmail.com>
2019-03-30 14:16:20 +1100

    The form will now be drawn via a Form class instead, which takes care of input validation and error feedback.

--- balancer/forms.py
+++ balancer/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-    
+########################################
 class StudentForm(forms.Form):
     first_name = forms.CharField(max_length=512, strip=True)
     surname = forms.CharField(max_length=512, strip=True)
     given_name = forms.CharField(max_length=512, strip=True)
     balance = forms.FloatField()
+########################################
+class StudentModifyForm(forms.Form):
+    new_balance = forms.FloatField(help_text = 'Enter the value to add/subtract from the balance (prefix the value with "-" for subtractions)')
