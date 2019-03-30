9a39860eb5d2f8cb0bf67a3fac25f5a76e10404e
Edmund Chong @ Banana Senpai <7440820@gmail.com>
2019-03-30 15:07:07 +1100

    Utility function for general purposes, consider migrating to a general purpose library. Num students can be calculated b counting the list of objects returned, so there's no need to make another DB call. Use a dictionary instead, manual comparison is just too tedious. Convert maunal regex to use functions. Code cleaning. Form class based view of the student details screen. First make sure that we have a valid ID, before redirecting to the processes.
    Signed-off-by: Edmund Chong @ Banana Senpai <7440820@gmail.com>

--- balancer/views.py
+++ balancer/views.py
@@ -5,12 +5,33 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 # model imports
 from balancer.models import Student
-from balancer.forms import StudentForm
+from balancer.forms import StudentForm, StudentModifyForm
 # python imports
 import sys
 import re
 ########################################
 ########################################
+# utility functions
+def IsEmpty(p_string):
+    # at least one non-space character
+    if not re.search("\S+", str(p_string)):
+        return True
+    return False;
+
+def ModelObjToPyDict(p_model_object):
+    py_dict = dict()
+    model_fields = p_model_object.get_iterable_fields()
+
+    for field in model_fields:
+        py_dict[field] = getattr(p_model_object, field)
+    return py_dict
+
+def IsNumber(p_value):
+    if re.search("\d+\.?\d*", str(p_value)):
+        return True
+    return False;
+########################################
+# views
 def index(request):
     all_students = Student.objects.all()
     num_students = len(all_students)
@@ -25,16 +46,21 @@ def index(request):
     )
 ########################################
 def index_plus(request, result):
-    num_students = Student.objects.all().count()
     all_students = Student.objects.all()
+    num_students = len(all_students)
 
     result_info = ''
 
-    if result.lower() == 'student-delete-success':
-        result_info = 'Student successfully deleted'
+    ERROR_DICT = {
+        'student-delete-success': 'Student successfully deleted',
+        'student-modform-error' : 'Student successfully deleted',
+    }
 
-    if not re.search("\S+", result_info):
-        result_info = False
+    if not IsEmpty(ERROR_DICT[result.lower()]):
+        result_info = ERROR_DICT[result.lower()]
+
+    if IsEmpty(result_info):
+        result_info = "Something went wrong when constructing the feedback message. Please contact your system administrator to troubleshoot this issue. Please check that your last actions actually took place."
 
     return render(
         request,
@@ -42,7 +68,7 @@ def index_plus(request, result):
         context={
         'num_students':num_students,
         'all_students':all_students,
-        'result_info':result_info,
+        'result_info' :result_info,
         },
     )
 ########################################
@@ -64,7 +90,7 @@ def StudentCreate(request):
 
         if form.is_valid():
             student_hash = {
-                'created_by':'edmundsuperadmin',
+                'created_by'  :'edmundsuperadmin',
                 'created_date':"2019-02-17"
             }
 
@@ -82,6 +108,22 @@ def StudentCreate(request):
 
     return render(request, 'balancer/student_form.html', context)
 ########################################
+def StudentModifyView(request, pk):
+    form = None
+    student_object = None
+
+    if not IsEmpty(pk):
+        form = StudentModifyForm(initial={'new_balance': 0.0})
+        student_object = Student.objects.get(pk=pk)
+    else:
+        return index_plus(request, 'student-modform-error')
+    context = {
+        'form': form,
+        'pk':pk,
+        'student':student_object
+    }
+    return render(request, 'balancer/student_detail.html', context)
+########################################
 def StudentDelete(request, student_obj):
     if not re.search("\S+", str(student_obj.pk)):
         return False
@@ -89,12 +131,32 @@ def StudentDelete(request, student_obj):
         student_obj.delete()
         return True
 ########################################
+def StudentCommitModify(request, student_obj):
+    print('test')
+    if not re.search("\S+", str(student_obj.pk)):
+        return False
+    else:
+        new_balance = request.POST['new_balance']
+        if not IsEmpty(new_balance) and IsNumber(new_balance):
+            # consider using a form class to build our modify form
+            # form = RenewBookForm(request.POST)
+            student_obj.balance = student_obj.balance + float(new_balance)
+            student_obj.balance = round(float(student_obj.balance), 2)
+            student_obj.save()
+        return True
+########################################
 def StudentModifyDelete(request):
     if request.method == 'POST':
-        if 'delete' in request.POST:
-            if 'student-id' in request.POST:
-                student_obj = Student.objects.get(pk=request.POST['student-id'])
+        student_obj = None
+
+        if 'student-id' in request.POST:
+            student_obj = Student.objects.get(pk=request.POST['student-id'])
+        if student_obj:
+            if 'delete' in request.POST:
                 if StudentDelete(request, student_obj):
                     return index_plus(request, 'student-delete-success')
+            elif 'modify' in request.POST:
+                if StudentCommitModify(request, student_obj):
+                    return HttpResponseRedirect( reverse('student-detail', args=[str(student_obj.pk)]) )
 
     return HttpResponseRedirect( reverse('index') )
